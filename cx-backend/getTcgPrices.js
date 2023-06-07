b7756bd1a93fb534cd0641be283c610db2970606
Martin Laureta <martin.laureta@live.com>
2023-06-07 19:37:06 +0800

    update backend endpoint

--- cx-backend/getTcgPrices.js
+++ cx-backend/getTcgPrices.js
@@ -7,8 +7,25 @@ import { getBearerToken, setPricing } from './utils.js';
 async function handler(event) {
     const bearer = await getBearerToken();
 
-    const productId = event.pathParameters.id
-    const multiplier = event.body
+    const eventBody = JSON.parse(event.body)
+    const productId = eventBody.id;
+    const multiplier = eventBody.multiplier;
+    
+    console.log(event.body)
+    console.log(eventBody)
+    console.log(productId)
+    console.log(multiplier)
+
+    const response = {
+        statusCode: 0,
+        body: ""
+    }
+
+    if(!Number.isInteger(multiplier)) {
+        response.statusCode = 400,
+        response.body = `Bad multiplier value, ${multiplier} is not a number.`
+        return response;
+    }
 
     var url = `https://api.tcgplayer.com/pricing/product/${productId}`
 
@@ -21,28 +38,28 @@ async function handler(event) {
     })
 
     const prices = await req.json();
-
-    const response = {
-        statusCode: 0,
-        body: ""
-    }
+    console.log(prices.success)
 
     if(prices.success) {
         response.statusCode = 200
         const priceData = {
             id: productId,
-            nonfoilMid: setPricing(prices.results.find((type) => type.subTypeName === 'Normal').midPrice, multiplier),
-            foilMid: prices.results.find((type) => type.subTypeName === 'Normal') != undefined 
+            nonfoilMid: prices.results.find((type) => type.subTypeName === 'Normal') != undefined 
                 ? setPricing(prices.results.find((type) => type.subTypeName === 'Normal').midPrice, multiplier)
+                : "No price available",
+            foilMid: prices.results.find((type) => type.subTypeName === 'Foil') != undefined 
+                ? setPricing(prices.results.find((type) => type.subTypeName === 'Foil').midPrice, multiplier)
                 : "No price available",
         }
 
         response.body = JSON.stringify(priceData, null, 2)
     } else {
         response.statusCode = 404
-        response.body = "Error"
+        response.body = prices.errors[0]
     }
     
+    console.log(response)
+
     return response;
 }
 
